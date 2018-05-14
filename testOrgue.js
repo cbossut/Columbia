1b418bd4691ce2ae4df07d571a150945850f6b2f
Unknown <bossut.clement@gmail.com>
2018-05-15 00:08:46 +0200

    and their tests

---
+++ testOrgue.js
@@ -0,0 +1,14 @@
+const cueList = require("./cueList.js")
+
+let cl = Object.create(cueList.proto)
+
+cl.orgue.patch = [
+  {pca:0,leds:[0]},
+  {pca:0,leds:[15]}
+]
+
+cl.load("./testOrgue.json")
+
+cl.print()
+
+cl.play()
