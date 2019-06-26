a8873de578d6d9731b9fd8cc60be9487c61f155b
Unknown <bossut.clement@gmail.com>
2019-06-26 12:14:09 +0200

    drag horizontal lines

--- WebInterface/columbox.js
+++ WebInterface/columbox.js
@@ -13,6 +13,10 @@ sur le site "http://www.cecill.info".
 
 const soundDur = 140
 
+const restrictToParent = interact.modifiers.restrict({
+  restriction: 'parent'
+})
+
 let CBlinesDiv = document.getElementById('CBlines')
 
 for ( let i in CBlines ) {
@@ -41,7 +45,11 @@ function boxulate(container, scenario) {
     switch ( box.func ) {
       case 'const':
         let line = newDiv('horizontalLine')
-        line.style.bottom = box.args[0] + '%'
+        line.updateValue = p => {
+          box.args[0] = p
+          line.style.bottom = p + '%'
+        }
+        line.updateValue(box.args[0])
         boxDiv.appendChild(line)
         break;
 
@@ -57,6 +65,18 @@ function boxulate(container, scenario) {
           , max = newDiv('horizontalLine')
         min.style.bottom = box.args[0] - box.args[1] + '%'
         max.style.bottom = box.args[0] + box.args[1] + '%'
+        min.updateValue = p => {
+          let maxP = box.args[0] + box.args[1]
+          box.args[0] = (p + maxP) / 2
+          box.args[1] = (maxP - p) / 2
+          min.style.bottom = p + '%'
+        }
+        max.updateValue = p => {
+          let minP = box.args[0] - box.args[1]
+          box.args[0] = (p + minP) / 2
+          box.args[1] = (p - minP) / 2
+          max.style.bottom = p + '%'
+        }
         boxDiv.appendChild(min)
         boxDiv.appendChild(max)
         break;
@@ -66,8 +86,27 @@ function boxulate(container, scenario) {
   }
 }
 
+interact('.horizontalLine').draggable({
+  lockAxis: 'y',
+  modifiers: [restrictToParent],
+  onmove: e => {
+    if ( e.dy ) transformFromEvent(e)
+  },
+  onend: e => {
+    let rect = e.target.parentNode.getBoundingClientRect()
+    e.target.style.transform = 'none'
+    e.target.updateValue(100 * (rect.bottom - e.client.y) / rect.height)
+  }
+})
+
 function newDiv(cl) {
   let d = document.createElement('div')
   if ( cl ) d.classList.add(cl)
   return d
+}
+
+function transformFromEvent(e) {
+  let totDx = (e.client.x - e.clientX0)
+    , totDy = (e.client.y - e.clientY0)
+  e.target.style.transform = 'translate(' + totDx + ',' + totDy + 'px)'
 }
